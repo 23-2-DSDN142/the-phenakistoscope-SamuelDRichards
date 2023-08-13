85f17ab8bc91fa1c6a3095700d86981b09e33b6e
SamuelDRichards <Sam@samrichardsstudio.com>
2023-08-14 11:02:07 +1200

    frog can jump
    set it up so the frog jumps, needs some work tho

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 20;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,44 +6,75 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image_sequence("frogTest" , "jpg", 3)
 }
+
+
+
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
-
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
-
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
-}
-
-function faces(x, y, animation, pScope){
+     new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
   
-  scale(animation.frame*2);
+     var layer1 = new PLayer(frogTest);
+     layer1.mode(RING)
+     layer1.set_boundary( 1000, 1000 )
+  
+ }
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
 
+
+function frogTest( x, y, animation, pScope) {
+  scale(.2)
+  pScope.draw_image_from_sequence("frogTest", 1000, -3000, animation.frame)
+  
 }
 
-function squares(x, y, animation, pScope){
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
-}
+
+
+
+
+
+// function setup_layers(pScope){
+
+//   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+
+//   var layer1 = new PLayer(faces);
+//   layer1.mode( SWIRL(5) );
+//   layer1.set_boundary( 200, 1000 );
+
+//   var layer2 = new PLayer(squares);
+//   layer2.mode( RING );
+//   layer2.set_boundary( 0, 400 );
+// }
+
+// function faces(x, y, animation, pScope){
+  
+//   scale(animation.frame*2);
+
+//   ellipse(0,0,50,50); // draw head
+//   fill(30);
+//   ellipse(-10,-10,10,10); //draw eye
+//   ellipse(10,-10,10,10); // draw eye
+//   arc(0,10,20,10,0,180); // draw mouth
+
+// }
+
+// function squares(x, y, animation, pScope){
+
+//   // this is how you set up a background for a specific layer
+//   let angleOffset = (360 / SLICE_COUNT) / 2
+//   let backgroundArcStart = 270 - angleOffset;
+//   let backgroundArcEnd = 270 + angleOffset;
+
+//   fill(66, 135, 245)
+//   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+
+//   fill(255)
+//   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+
+// }

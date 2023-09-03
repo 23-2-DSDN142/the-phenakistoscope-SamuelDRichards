fad13c2af85e69cf6359c656f2f138b9abf14ca7
SamuelDRichards <Sam@samrichardsstudio.com>
2023-09-04 08:38:21 +1200

    Lily pad
    added a lily pad

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -4,10 +4,11 @@ function setup_pScope(pScope){
   //pScope.output_mode(ANIMATED_DISK);
   pScope.output_mode(STATIC_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("frogTest" , "png", 14)
+  angleMode(DEGREES);
 }
 
 
@@ -21,10 +22,15 @@ function setup_layers(pScope){
   
 
      new PLayer(null, 179,85,133);  //lets us draw the whole circle background, ignoring the boundaries
-  
+
+    
+
      var layer1 = new PLayer(frogTest);
      layer1.mode(RING)
      layer1.set_boundary( 10, 10 )
+
+     var layer2 = new PLayer(lilypad);
+     layer2.mode(RING)
   
  }
 
@@ -37,10 +43,23 @@ function frogTest( x, y, animation, pScope) {
 }
 
 
-function block(x, y, animation, pScope) {
+function lilypad( x, y, animation, pScope) {
+  
+  strokeWeight(0);
+  fill(10,77,1)
+  arc(x,y, 400, 400, 0, 360)
+
+  fill(179,85,133) //background pink
+  triangle(10, 10, 160, 130, 130, 160 )
   
 }
 
+
+function reeds(x, y, animation, pScopeams) {
+
+
+  
+}
 
 
 

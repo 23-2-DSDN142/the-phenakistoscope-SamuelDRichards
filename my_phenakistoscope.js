34173bfad54d5e5638786e0d67dda3a83b09ed9e
SamuelDRichards <Sam@samrichardsstudio.com>
2023-09-10 18:38:36 +1200

    add flower
    added a flower but you cant see it and I dont know how to fix it

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -4,11 +4,13 @@ function setup_pScope(pScope){
   //pScope.output_mode(ANIMATED_DISK);
   pScope.output_mode(STATIC_DISK);
   //pScope.output_mode(STATIC_FRAME)
+  //pScope.output_mode(OUTPUT_GIF(1000))
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("frogTest" , "png", 14)
+  pScope.load_image("flower" , "png");
   angleMode(DEGREES);
 }
 
@@ -25,16 +27,18 @@ function setup_layers(pScope){
      new PLayer(null, 179,85,133);  //lets us draw the whole circle background, ignoring the boundaries
 
     
-
+    
      var layer1 = new PLayer(frogTest);
      layer1.mode(RING)
      layer1.set_boundary( 0, 1000 )
 
-     var layer2 = new PLayer(lilypad);
-     layer2.mode(RING)
+      var layer2 = new PLayer(flowers);
+      layer2.mode(RING)
 
-    //  var layer3 = new PLayer(reeds);
-    //  layer3.mode(RING)
+     var layer3 = new PLayer(lilypad);
+     layer3.mode(RING)
+
+  
     
     
  }
@@ -46,6 +50,20 @@ function frogTest( x, y, animation, pScope) {
   pScope.draw_image_from_sequence("frogTest", 0, -4000, animation.frame)
   
 }
+
+
+
+function flowers( x, y, animation, pScope) {
+  scale(.03)
+  if(animation.frame == 0){
+    pScope.draw_image("flower",-500,-1000);
+  }
+
+}
+
+
+
+
 
 
 function lilypad( x, y, animation, pScope) {
@@ -86,6 +104,8 @@ function lilypad( x, y, animation, pScope) {
 
   
 }
+
+
 
 
 
